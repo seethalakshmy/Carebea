72b0742fbfb338be21a366d012fbfbb817bd8247
anand-seeroo <anand.s@seeroo.com>
2022-07-01 15:27:07 +0530

    added colors and themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:carebea/app/utils/theme.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -11,12 +12,27 @@ void main() {
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,),
-        textTheme: TextTheme(
-          headlineLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black)
-        )
+      theme: ThemeData.light().copyWith(
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.white, elevation: 0),
+        extensions: [
+          const CustomTheme(
+            primary: Color(0xffB00069),
+            secondary: Color(0xff0098BA),
+            action: Color(0xff1CA645),
+            cardBackground: Color(0xffFFF1F1),
+            shadowColor: Color(0x1A000000),
+            regular: TextStyle(
+              fontFamily: "Poppins",
+              fontWeight: FontWeight.w500,
+              color: Color(0xff111111),
+            ),
+            medium: TextStyle(
+              fontFamily: "Poppins",
+              fontWeight: FontWeight.w400,
+              color: Color(0xff111111),
+            ),
+          ),
+        ],
       ),
     ),
   );
