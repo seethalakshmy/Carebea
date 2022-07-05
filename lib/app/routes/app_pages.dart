866a90971197aa6245f9356afe456df5ed96eda4
anand-seeroo <anand.s@seeroo.com>
2022-07-05 18:55:42 +0530

    changed to dashboard

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -24,7 +24,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.ROUTE_PAGE;
+  static const INITIAL = Routes.DASHBOARD;
 
   static final routes = [
     GetPage(
