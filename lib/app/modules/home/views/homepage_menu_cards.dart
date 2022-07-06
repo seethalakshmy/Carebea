faf710b07ede2cb46c7d148d133409c4c53134c2
anand-seeroo <anand.s@seeroo.com>
2022-07-06 11:01:58 +0530

    done menu cards

--- lib/app/modules/home/views/homepage_menu_cards.dart
+++ lib/app/modules/home/views/homepage_menu_cards.dart
@@ -1,4 +1,5 @@
 import 'package:carebea/app/utils/assets.dart';
+import 'package:carebea/app/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -7,23 +8,44 @@ class HomeMenuCards extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Container(
-      height: Get.size.height*.3,
+      height: Get.size.height * .3,
       child: GridView(
-          gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-            childAspectRatio: 12/8
-          ),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, crossAxisSpacing: 10, mainAxisSpacing: 10, childAspectRatio: 12 / 8),
         children: [
-          HomeMenuIndividual(),
-          HomeMenuIndividual(),
-          HomeMenuIndividual(),
-          HomeMenuIndividual(),
-          HomeMenuIndividual(),
-          HomeMenuIndividual(),
+          HomeMenuIndividual(
+            asseticon: Assets.shopIcon,
+            gradients: [Color(0xff0098BA), Color(0xff4163A2)],
+            title: "Create new order",
+          ),
+          HomeMenuIndividual(
+            asseticon: Assets.shopIcon,
+            gradients: [Color(0xffA73B6E), Color(0xff985194)],
+            title: "Add new shop",
+          ),
+          HomeMenuIndividual(
+            asseticon: Assets.shopIcon,
+            gradients: [Color(0xff66DE9D), Color(0xff00B2BE)],
+            title: "Reports",
+          ),
+          HomeMenuIndividual(
+            asseticon: Assets.shopIcon,
+            backgroundColor: Color(0xffF74254),
+            title: "Route",
+          ),
+          HomeMenuIndividual(
+            asseticon: Assets.shopIcon,
+            backgroundColor: Color(0xffD8375C),
+            title: "Today's Delivery",
+            count: 1,
+          ),
+          HomeMenuIndividual(
+            asseticon: Assets.shopIcon,
+            backgroundColor: Color(0xffF3674F),
+            title: "Total Orders",
+            count: 89,
+          ),
         ],
       ),
     );
@@ -31,46 +53,91 @@ class HomeMenuCards extends StatelessWidget {
 }
 
 class HomeMenuIndividual extends StatelessWidget {
-  const HomeMenuIndividual({Key? key}) : super(key: key);
+  const HomeMenuIndividual({
+    Key? key,
+    this.asseticon,
+    this.backgroundColor,
+    this.gradients,
+    required this.title,
+    this.onTap,
+    this.count,
+  }) : super(key: key);
+
+  final List<Color>? gradients;
+  final Color? backgroundColor;
+  final String title;
+  final String? asseticon;
+  final int? count;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12.0),
-        border: Border.all(color: Colors.black)
-      ),
-      child: Stack(
-        children: [
-          Positioned(
-
-            child: Image.asset(Assets.shopIcon, width: 40, height: 40,),
-            right: -10,
-            top: 18,
-          ),
-          Opacity(
-            opacity: .6,
-            child: Container(
-
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12.0),
-                gradient: LinearGradient(colors:  [Colors.blue.shade600, Colors.blue.shade900])
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(12.0),
+          gradient: gradients != null ? LinearGradient(colors: gradients!) : null,
+        ),
+        child: Builder(builder: (context) {
+          if (count != null) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Create New Order"),
+                _title(context),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: Text(
+                        count.toString(),
+                        style: customTheme(context).regular.copyWith(
+                              fontSize: 28,
+                              color: Colors.white,
+                            ),
+                      ),
+                    ),
+                    _icon(),
+                  ],
+                )
               ],
-            ),
-          ),
+            );
+          }
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                child: _title(context),
+              ),
+              _icon(),
+            ],
+          );
+        }),
+      ),
+    );
+  }
 
-        ],
+  Image _icon() {
+    return Image.asset(
+      asseticon!,
+      width: 40,
+      height: 40,
+      color: Colors.white24,
+    );
+  }
+
+  Widget _title(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        title,
+        style: customTheme(context).medium.copyWith(
+              fontSize: 10,
+              color: Colors.white,
+            ),
       ),
     );
   }
 }
-
