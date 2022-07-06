42990bb3ff2ac42fbcc2d0245f251fdd0035f754
Najibvenkitta <najibvenkitta10@gmail.com>
2022-07-06 11:10:49 +0530

    profile ui

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -26,7 +26,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.ORDERS;
+  static const INITIAL = Routes.DASHBOARD;
 
   static final routes = [
     GetPage(
