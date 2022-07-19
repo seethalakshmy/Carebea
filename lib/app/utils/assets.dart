517538def97e293eb4f7d314baf66641bcb8a7de
anand-seeroo <anand.s@seeroo.com>
2022-07-19 12:10:48 +0530

    order icon fixed

--- lib/app/utils/assets.dart
+++ lib/app/utils/assets.dart
@@ -6,7 +6,7 @@ class Assets {
   static const String assetsSplashImage = 'assets/images/splash_image.png';
   static const String homeIcon = 'assets/images/home.png';
   static const String shopIcon = 'assets/images/shop.png';
-  static const String orderIcon = 'assets/images/order.png';
+  static const String orderIcon = 'assets/images/order-nav.png';
   static const String mapPinRound = 'assets/images/map_pin_round.png';
   static const String location = 'assets/images/location.png';
   static const String assetsLocationFilled = 'assets/images/location_filled.png';
