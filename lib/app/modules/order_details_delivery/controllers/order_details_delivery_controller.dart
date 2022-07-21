241357dbed55644d86cf8595009fc88450c4d6aa
anand-seeroo <anand.s@seeroo.com>
2022-07-22 01:28:11 +0530

    order details

--- lib/app/modules/order_details_delivery/controllers/order_details_delivery_controller.dart
+++ lib/app/modules/order_details_delivery/controllers/order_details_delivery_controller.dart
@@ -1,23 +1,23 @@
+import 'package:carebea/app/modules/shops/models/order_list_model.dart';
+import 'package:carebea/app/modules/shops/repo/order_list_repo.dart';
+import 'package:carebea/app/utils/shared_prefs.dart';
 import 'package:get/get.dart';
 
 class OrderDetailsDeliveryController extends GetxController {
-  //TODO: Implement OrderDetailsDeliveryController
+  OrderListRepo orderListRepo = OrderListRepo();
+  OrderListResponse? orderListDetailResponse;
+  RxBool isOrderDetailsLoading = true.obs;
 
-  final count = 0.obs;
   @override
   void onInit() {
+    fetchOrderDetails(Get.arguments['order_id']);
     super.onInit();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  fetchOrderDetails(int orderId) async {
+    isOrderDetailsLoading(true);
+    orderListDetailResponse =
+        await orderListRepo.orderDetails(salesPersonId: SharedPrefs.getUserId(), orderId: orderId);
+    isOrderDetailsLoading(false);
   }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
