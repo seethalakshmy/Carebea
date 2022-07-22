b2c329b53fee3d180b4a27742285324772811dbc
anand-seeroo <anand.s@seeroo.com>
2022-07-22 17:06:09 +0530

    model generated

--- lib/app/modules/delivery_invoice_details/data/models/invoice_details_response.g.dart
+++ lib/app/modules/delivery_invoice_details/data/models/invoice_details_response.g.dart
@@ -6,19 +6,28 @@ part of 'invoice_details_response.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-InvoiceDetailsResponse _$InvoiceDetailsResponseFromJson(Map<String, dynamic> json) => InvoiceDetailsResponse(
+InvoiceDetailsResponse _$InvoiceDetailsResponseFromJson(
+        Map<String, dynamic> json) =>
+    InvoiceDetailsResponse(
       jsonrpc: json['jsonrpc'] as String?,
       id: json['id'],
-      result: json['result'] == null ? null : InvoiceDetailsResult.fromJson(json['result'] as Map<String, dynamic>),
+      result: json['result'] == null
+          ? null
+          : InvoiceDetailsResult.fromJson(
+              json['result'] as Map<String, dynamic>),
     );
 
-Map<String, dynamic> _$InvoiceDetailsResponseToJson(InvoiceDetailsResponse instance) => <String, dynamic>{
+Map<String, dynamic> _$InvoiceDetailsResponseToJson(
+        InvoiceDetailsResponse instance) =>
+    <String, dynamic>{
       'jsonrpc': instance.jsonrpc,
       'id': instance.id,
       'result': instance.result,
     };
 
-InvoiceDetailsResult _$InvoiceDetailsResultFromJson(Map<String, dynamic> json) => InvoiceDetailsResult(
+InvoiceDetailsResult _$InvoiceDetailsResultFromJson(
+        Map<String, dynamic> json) =>
+    InvoiceDetailsResult(
       orderId: json['order_id'] as int?,
       invoiceId: json['invoice_id'] as int?,
       userAddress: json['user_address'] as String?,
@@ -31,7 +40,9 @@ InvoiceDetailsResult _$InvoiceDetailsResultFromJson(Map<String, dynamic> json) =
       message: json['message'] as String?,
     );
 
-Map<String, dynamic> _$InvoiceDetailsResultToJson(InvoiceDetailsResult instance) => <String, dynamic>{
+Map<String, dynamic> _$InvoiceDetailsResultToJson(
+        InvoiceDetailsResult instance) =>
+    <String, dynamic>{
       'order_id': instance.orderId,
       'invoice_id': instance.invoiceId,
       'user_address': instance.userAddress,
