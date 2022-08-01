8f3c84d31b46b5a528aa9739cfd77d67f536d430
anand-seeroo <anand.s@seeroo.com>
2022-08-01 16:35:09 +0530

    added qr validation

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -6,6 +6,7 @@ import 'package:carebea/app/modules/home/views/latest_shops_added_view.dart';
 import 'package:carebea/app/modules/home/widgets/search_widget.dart';
 import 'package:carebea/app/modules/shops/views/shop_details.dart';
 import 'package:carebea/app/routes/app_pages.dart';
+import 'package:carebea/app/utils/show_snackbar.dart';
 import 'package:carebea/app/utils/theme.dart';
 import 'package:carebea/app/utils/widgets/appbar.dart';
 import 'package:carebea/app/utils/widgets/circular_progress_indicator.dart';
@@ -25,19 +26,18 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: appBar(context, showScanner: true, onScanned: (val) {
-        try  {
+          try {
             var qr = QrResponse.fromJson(json.decode(val.replaceAll("\'", "\"")));
             if (qr.type == 1) {
               Get.to(() => ShopDetails(
                     shopId: qr.id,
                   ));
             } else if (qr.type == 2) {
-              Get.toNamed(Routes.ORDER_HISTORY_DETAILS,
-                  arguments: {'order_id': qr.id});
+              Get.toNamed(Routes.ORDER_HISTORY_DETAILS, arguments: {'order_id': qr.id});
             }
-          }
-          catch(e,s){
-          developer.log('error',error: e,stackTrace: s);
+          } catch (e, s) {
+            developer.log('error', error: e, stackTrace: s);
+            showSnackBar("Please scan again");
           }
         }),
 
@@ -91,10 +91,9 @@ class HomeView extends GetView<HomeController> {
               padding: EdgeInsets.only(top: 20, left: 15.0, right: 15.0),
               sliver: SliverToBoxAdapter(child: HomeMenuCards()),
             ),
-          const  SliverPadding(
+            const SliverPadding(
                 padding: EdgeInsets.only(bottom: 10),
-                sliver: SliverToBoxAdapter(
-                    child: HomepageUpcomingDeliveryView())),
+                sliver: SliverToBoxAdapter(child: HomepageUpcomingDeliveryView())),
             const SliverToBoxAdapter(child: HomepageLatestShopsAddedView()),
           ]);
         }));
