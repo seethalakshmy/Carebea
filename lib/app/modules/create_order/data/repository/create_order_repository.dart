7a4d21e55588f90731adaca0e64f6fa9a65697a3
anand-seeroo <anand.s@seeroo.com>
2022-09-23 16:58:44 +0530

    create order fixed

--- lib/app/modules/create_order/data/repository/create_order_repository.dart
+++ lib/app/modules/create_order/data/repository/create_order_repository.dart
@@ -5,8 +5,8 @@ import 'package:carebea/app/modules/create_order/model/create_order.dart';
 class CreateOrderRepository {
   final CreateorderDataSource _dataSource = CreateorderDataSource();
 
-  Future<CreateOrderResponse> createOrder({int? salesPersonId, int? shopId, Map<int, int>? products}) =>
-      _dataSource.createOrder(salesPersonId!, shopId!, products!);
+  Future<CreateOrderResponse> createOrder({int? salesPersonId, int? shopId, Map<int, int>? products,int? orderId}) =>
+      _dataSource.createOrder(salesPersonId!, shopId!, products!,orderId);
 
   Future<ConfirmOrderResponse> confirmOrder(
    {required int salesPersonId,
